f3564a09901e2f29158608f7999b0fea202abd41
Sergey <grechin.sergey@gmail.com>
2020-05-18 10:27:16 +0300

    progressing with cython part a bit

--- cython_vst_loader/vst_loader_wrapper.pyx
+++ cython_vst_loader/vst_loader_wrapper.pyx
@@ -1,14 +1,41 @@
+from typing import Callable
+
+# @formatter:off
 from posix.dlfcn cimport dlopen, dlsym, RTLD_LAZY
 
+from cython_vst_loader.vst_plugin import VstPluginInstance
+
+# @formatter:on
+
 cdef extern from "aeffectx.h":
-    pass
+    ctypedef struct AEffect
+    ctypedef VstInt32
+    ctypedef VstIntPtr
+    ctypedef VstIntPtr (*audioMasterCallback)(AEffect*effect, VstInt32 opcode, VstInt32 index, VstIntPtr value,
+                                              void*ptr, float opt);
+    ctypedef int64_t
+
+ctypedef AEffect *(*vstPluginFuncPtr)(audioMasterCallback host);
 
 def raise_error(message):
     raise message
 
-cdef void* load_vst(int a):
+plugin_callback_map = {}
 
-    cdef char* entry_function_name = "VSTPluginMain"
-    cdef void* handle = dlopen("/c/path/to/vst.so", RTLD_LAZY)
+cdef VstIntPtr host_callback(AEffect*effect, VstInt32 opcode, VstInt32 index, VstIntPtr value, void*ptr, float opt):
+    # return 123, aways. make it work
+    pass
 
-    return handle
+cdef AEffect*load_vst(char*path_to_so, python_host_callback):
+    cdef char*entry_function_name = "VSTPluginMain"
+    cdef void*handle = dlopen(path_to_so, RTLD_LAZY)
+    cdef vstPluginFuncPtr entry_function = <vstPluginFuncPtr> dlsym(handle, "VSTPluginMain")
+
+    cdef AEffect*plugin_ptr = entry_function(host_callback)
+
+    plugin_callback_map[<long> plugin_ptr] = python_host_callback
+    return plugin_ptr
+
+def create_plugin(path_to_so: bytes, python_host_callback: Callable)-> VstPluginInstance:
+    plugin_pointer = load_vst(path_to_so, python_host_callback)
+    plugin = VstPluginInstance(<long> plugin_pointer)
--- cython_vst_loader/vst_plugin.py
+++ cython_vst_loader/vst_plugin.py
@@ -1,2 +1,3 @@
-class VstPlugin:
-    pass
+class VstPluginInstance:
+    def __init__(self, plugin_pointer):
+        self._plugin_pointer = plugin_pointer
