f2006e5d4c25b7d99c3dba65344d6679c8917191
Sergey Grechin <grechin.sergey@gmail.com>
2021-03-22 22:59:09 +0300

    another change in order

--- build_scripts/build_on_host.sh
+++ build_scripts/build_on_host.sh
@@ -14,14 +14,14 @@ set -e
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_DIR="${SCRIPT_DIR}/../"
 
+echo "about to build for 3.9 manylinux"
+docker run --rm -t -v `pwd`:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp39-cp39
+echo "done building for 3.9 manylinux"
+
 rm -rf ${PROJECT_DIR}/dist/*
 echo "about to build for 3.7 manylinux"
 docker run --rm -t -v `pwd`:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp37-cp37m
 echo "done building for 3.7 manylinux"
-
-echo "about to build for 3.9 manylinux"
-docker run --rm -t -v `pwd`:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp39-cp39
-echo "done building for 3.9 manylinux"
 
 
 echo "about to build for 3.8 manylinux"
