f1c52a9e2e71f1f4caf702b0c868ecff76157cf5
Sergey Grechin <grechin.sergey@gmail.com>
2021-03-22 22:21:40 +0300

    adjusting volumes

--- build_scripts/build_on_host.sh
+++ build_scripts/build_on_host.sh
@@ -16,13 +16,13 @@ PROJECT_DIR="${SCRIPT_DIR}/../"
 
 rm -rf ${PROJECT_DIR}/dist/*
 echo "about to build for 3.7 manylinux"
-docker run --rm -t -v ~/em/cython-vst-loader:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp37-cp37m
+docker run --rm -t -v .:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp37-cp37m
 echo "done building for 3.7 manylinux"
 
 echo "about to build for 3.8 manylinux"
-docker run --rm -it -v ~/em/cython-vst-loader:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp38-cp38
+docker run --rm -t -v .:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp38-cp38
 echo "done building for 3.8 manylinux"
 
 echo "about to build for 3.9 manylinux"
-docker run --rm -it -v ~/em/cython-vst-loader:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp39-cp39
+docker run --rm -t -v .:/cython-vst-loader quay.io/pypa/manylinux2010_x86_64:latest bash /cython-vst-loader/build_scripts/build_in_manylinux_container.sh cp39-cp39
 echo "done building for 3.9 manylinux"
