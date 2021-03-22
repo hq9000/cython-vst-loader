28eb7f0abdd50eaac1abdd0cbc1dfdab8a600456
Sergey Grechin <grechin.sergey@gmail.com>
2021-03-22 22:44:43 +0300

    cache + debug

--- build_scripts/build_in_manylinux_container.sh
+++ build_scripts/build_in_manylinux_container.sh
@@ -26,6 +26,7 @@ python setup.py build_ext --inplace
 python setup.py bdist_wheel
 
 mkdir -p /cython-vst-loader/dist/manylinux
+ls -la /cython-vst-loader/dist
 auditwheel repair --plat manylinux1_x86_64 --wheel-dir /cython-vst-loader/dist/manylinux /cython-vst-loader/dist/*.whl
 #rm -f /cython-vst-loader/dist/*.whl
 deactivate
